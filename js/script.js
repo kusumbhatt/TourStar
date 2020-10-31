b10b44359138fb42c990f6b6f46516a42309c9cb
kusumbhatt <kbhatt0701@gmail.com>
2020-11-01 01:25:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    /* mobile-nav*/   
+    $('.toggle-btn').click(function(){
+        var nav = $('.nav-links');
+        var icon = $('.toggle-btn i');
+        if(nav.hasClass('open-btn'))
+            nav.removeClass('open-btn');
+        else
+            nav.addClass('open-btn');
+
+        if(icon.hasClass('fa-bars')){
+            icon.addClass('fa-times');
+            icon.removeClass('fa-bars');
+        }else{
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+    });
+})();
