e38a2ae846467188cf0ac0be6dfc9b1aa57bb2a1
kusumbhatt <kbhatt0701@gmail.com>
2020-11-10 10:44:48 +0530

    fixed toggle-btn

--- js/script.js
+++ js/script.js
@@ -17,3 +17,20 @@ $(document).ready(function(){
         }
     });
 });
+
+$(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth > 767) {
+        var nav = $('.nav-links');
+        var icon = $('.toggle-btn i');
+        if(nav.hasClass('open-btn'))
+            nav.removeClass('open-btn');
+
+        if(icon.hasClass('fa-bars')){
+            
+        }else{
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+    }
+});
