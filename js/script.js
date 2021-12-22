a6af13a6adf755e29e99f9bf200f4848f75f9014
kusumbhatt <kbhatt0701@gmail.com>
2021-12-22 09:25:55 +0530

    css-formatting

--- js/script.js
+++ js/script.js
@@ -1,36 +1,32 @@
-$(document).ready(function(){
-    /* mobile-nav*/   
-    $('.toggle-btn').click(function(){
-        var nav = $('.nav-links');
-        var icon = $('.toggle-btn i');
-        if(nav.hasClass('open-btn'))
-            nav.removeClass('open-btn');
-        else
-            nav.addClass('open-btn');
+$(document).ready(function () {
+  /* mobile-nav*/
+  $(".toggle-btn").click(function () {
+    let nav = $(".nav-links");
+    let icon = $(".toggle-btn i");
+    if (nav.hasClass("open-btn")) nav.removeClass("open-btn");
+    else nav.addClass("open-btn");
 
-        if(icon.hasClass('fa-bars')){
-            icon.addClass('fa-times');
-            icon.removeClass('fa-bars');
-        }else{
-            icon.addClass('fa-bars');
-            icon.removeClass('fa-times');
-        }
-    });
+    if (icon.hasClass("fa-bars")) {
+      icon.addClass("fa-times");
+      icon.removeClass("fa-bars");
+    } else {
+      icon.addClass("fa-bars");
+      icon.removeClass("fa-times");
+    }
+  });
 });
 
 $(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth > 767) {
-        var nav = $('.nav-links');
-        var icon = $('.toggle-btn i');
-        if(nav.hasClass('open-btn'))
-            nav.removeClass('open-btn');
+  var viewportWidth = $(window).width();
+  if (viewportWidth > 767) {
+    var nav = $(".nav-links");
+    var icon = $(".toggle-btn i");
+    if (nav.hasClass("open-btn")) nav.removeClass("open-btn");
 
-        if(icon.hasClass('fa-bars')){
-            
-        }else{
-            icon.addClass('fa-bars');
-            icon.removeClass('fa-times');
-        }
+    if (icon.hasClass("fa-bars")) {
+    } else {
+      icon.addClass("fa-bars");
+      icon.removeClass("fa-times");
     }
+  }
 });
